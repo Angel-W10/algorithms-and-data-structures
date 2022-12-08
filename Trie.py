5bd7d672d18f4d9a40cc40702796d938dafbba5b
Angel-W10 <angelwadhawan10@gmail.com>
2022-12-09 02:00:30 +1030

    insert and search working

--- Trie.py
+++ Trie.py
@@ -1,51 +1,49 @@
 from collections import defaultdict
 
-class node():
-    def __init__(self, val = " "):
+class node:
+    def __init__(self, val):
         self.val = val
-        self.children = defaultdict(list)
+        self.is_end = False
+        self.children = {}
 
 
-class Trie:
+class Trie(object):
 
     def __init__(self):
-        self.root = node()
-        # self.children = []
+        self.root = node(" ")
         
 
     def insert(self, word: str) -> None:
-        # if not word: return False
+    #    if not word: return False
+       cur = self.root
+
+       for c in word:
+        if(c in cur.children):
+            cur = cur.children[c]
+        else:
+            new_node = node(c)
+            cur.children[c] = new_node
+            cur = new_node
+        cur.is_end = True
+
+
+                
+                
+    def search(self, word):
+
         cur = self.root
+
         for c in word:
-            # print(c)
-            if len(cur.children) == 0:
-                print("inserting")
-                cur.children[ord(c)-97] = node(c)
-            cur = cur.children[ord(c)-97]
-                
-                
-        
+            if(c in cur.children):
+                cur = cur.children[c]
+            else:
+                return False
+        return True
 
-    def search(self, word: str) -> bool:
-        if not word: return False
-        
-        
-
-    def startsWith(self, prefix: str) -> bool:
-        pass
-    
-    
-
-
-
-# Your Trie object will be instantiated and called as such:
-# obj = Trie()
-# obj.insert(word)
-# param_2 = obj.search(word)
-# param_3 = obj.startsWith(prefix)
 
 
 if __name__ == "__main__":
     t = Trie()
     t.insert("and")
-    print(len(t.root.children))
+    t.insert("anc")
+    print(t.search("anc"))
